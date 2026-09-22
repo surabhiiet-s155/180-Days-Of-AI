87b26e2b123804afbc43a529ba10f5ea7d55ae4a
Surabhi <asurabhisubhaschandra@gmail.com>
2026-09-22 22:50:51 +0530

    Remove extra newline in TFIDF_TextSimilarity.py

--- Day32/TFIDF_TextSimilarity.py
+++ Day32/TFIDF_TextSimilarity.py
@@ -1,6 +1,5 @@
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.metrics.pairwise import cosine_similarity
-
 documents = [
     "I love learning Python and machine learning",
     "Python and machine learning are interesting",
